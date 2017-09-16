e56fe3678edbb448255b4cfd392b2abed1ee29e8
devid.kohut@gmail.com <rhfobqgfhjkmyfcdsns>
2017-09-16 17:39:58 +0300

    ☄️

---
+++ app.js
@@ -0,0 +1,30 @@
+const app = {
+    selector: 'app',
+    template: '<navigation></navigation><view></view>'
+};
+
+const nav = {
+    selector: 'navigation',
+    template: '<a href="/#/"> Main</a> <a href="/#/second"> Second</a>'
+};
+
+const mainPage = {
+    route: '/',
+    selector: 'main-page',
+    template: '<h1>Main Page</h1> <pre>{{ test }}</pre>',
+    script: function () {
+        // x(this.selector).html(x(this.selector).html().replace('{{ test }}', 'test'));
+        x(this.selector).html(this.template.replace('{{ test }}', 'lolo'))
+    }
+};
+
+const secondPage = {
+    route: '/second',
+    selector: 'second-page',
+    template: '<h1>Second Page</h1>'
+};
+
+new Yavir({
+    el: 'app',
+    components: [app, nav, mainPage, secondPage]
+}).run();
